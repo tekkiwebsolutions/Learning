93b8c142463cddd391187558c17fddc3658c8994
tekkiwebsolutions <sarbjitgrewal@tekkiwebsolutions.com>
2020-09-15 18:09:17 +0530

    changes done

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -2,24 +2,25 @@ import React from "react";
 import { Container, Nav, Navbar } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '../../fonts/fonts.css';
-import logo from '../../images/logo.png'
+import logo from '../../images/logo.png';
+import { Link } from "gatsby";
 
 const Header = () => (
 	<header>
 		<Container>
 		    <Navbar expand="lg">
-			  <Navbar.Brand href="#home"><img src={logo} alt="logo" /></Navbar.Brand>
+			  <Navbar.Brand><Link to="/"><img src={logo} alt="logo" /></Link></Navbar.Brand>
 			  <Navbar.Toggle aria-controls="basic-navbar-nav" />
 			  <Navbar.Collapse id="basic-navbar-nav">
 			    <Nav className="">
-			      <Nav.Link href="#home">Study Support</Nav.Link>
-			      <Nav.Link href="#link">Tutoring</Nav.Link>
-			      <Nav.Link href="#home">Online Courses</Nav.Link>
-			      <Nav.Link href="#link">About Us</Nav.Link>
+			      <Link to="/study-support" activeClassName="active">Study Support</Link>
+			      <Link to="/tutoring" activeClassName="active">Tutoring</Link>
+			      <Link to="/" activeClassName="active">Online Courses</Link>
+			      <Link to="/about" activeClassName="active">About Us</Link>
 			    </Nav>
 			  </Navbar.Collapse>
 			  <div className="">
-	      		<Nav.Link href="#deets" className="get-started-btn">Get Started</Nav.Link>
+			 	<Link to="/contact" className="get-started-btn">Get Started</Link>
 			  </div>
 			</Navbar>
 		</Container>

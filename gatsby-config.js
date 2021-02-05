7de55a4c95122caf4a78bea87f648ee0ea7c0444
Tekki web solutions <sarbjit@tekkiwebsolutions.com>
2021-02-05 13:43:49 +0530

    Revert "Learning about changes"
    This reverts commit 22497d418b465b0b674c76b3680254ddafcd3e0b.

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,22 +3,8 @@
  *
  * See: https://www.gatsbyjs.org/docs/gatsby-config/
  */
-const dotenv = require('dotenv')
-  if(process.env.NODE_ENV != 'production'){
-    dotenv.config()
-  }
+
 module.exports = {
   /* Your site config here */
-  plugins: [
-    `gatsby-plugin-sharp`,
-    {
-      resolve:`gatsby-source-contentful`,
-      options:{
-          spaceId: `lnno1lx6r2nh`,
-          accessToken:process.env.CONTENTFUL_ACCESS_TOKEN
-      }
-
-    }
-    // `@contentful/gatsby-transformer-contentful-richtext`
-  ],
+  plugins: [],
 }

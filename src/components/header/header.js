674b79daa71ae92b2517264ee873666b45c90ee6
Tekki web solutions <sarbjit@tekkiwebsolutions.com>
2021-02-08 14:05:43 +0530

    add box shadow

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -9,20 +9,18 @@ const Header = () => (
 	<header>
 		<Container>
 		    <Navbar expand="lg">
-			  <Navbar.Brand><Link to="/"><img src={logo} alt="logo" /></Link></Navbar.Brand>
-			  <Navbar.Toggle aria-controls="basic-navbar-nav" />
-			  <Navbar.Collapse id="basic-navbar-nav">
-			    <Nav className="">
-			      <Link to="/study-support" activeClassName="active">Study Support</Link>
-			      <Link to="/tutoring" activeClassName="active">Tutoring</Link>
-			      <Link to="/about" activeClassName="active">About Us</Link>
-			      <Link to="/contact" className="get-started-btn box-shadow">Get Started</Link>
-			    </Nav>
-			  </Navbar.Collapse>
-			 
+					<Navbar.Brand><Link to="/"><img src={logo} alt="logo" /></Link></Navbar.Brand>
+					<Navbar.Toggle aria-controls="basic-navbar-nav" />
+					<Navbar.Collapse id="basic-navbar-nav">
+						<Nav className="">
+							<Link to="/study-support" activeClassName="active">Study Support</Link>
+							<Link to="/tutoring" activeClassName="active">Tutoring</Link>
+							<Link to="/about" activeClassName="active">About Us</Link>
+							<Link to="/contact" className="get-started-btn box-shadow">Get Started</Link>
+						</Nav>
+					</Navbar.Collapse>
 			</Navbar>
 		</Container>
 	</header>
- 
 )
 export default Header
